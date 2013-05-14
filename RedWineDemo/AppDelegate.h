78b9df605adb95fb435d9fa969719d03c7957af9
xyhh <niko@localhost.(none)>
2013-05-14 16:31:00 -0700

    Initial Commit

---
+++ RedWineDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RedWineDemo
+//
+//  Created by xyhh on 13-5-14.
+//  Copyright (c) 2013年 bill wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
